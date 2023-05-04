83aa9d61c94504209ecc86c1eab85cb7bc6aab3b
Flor <florenciacuccarese@gmail.com>
2023-05-04 20:43:16 -0300

    los botones funcionan pero les falta amor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestPage from './pages/testPage.js';
+import QuestionPage from './pages/questionPage.js';
 
 
 import {
@@ -13,6 +14,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<TestPage/>}/>
+      <Route path="/question" element={<QuestionPage/>}/>
+
 
     </Routes>
   </BrowserRouter>,
